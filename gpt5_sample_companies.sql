f71485dd18ced16feb6179167d178a6ca9761ce7
Nhat Minh Phan <s3978598@rmit.edu.vn>
2023-04-20 10:09:22 +0700

    kk

---
+++ gpt5_sample_companies.sql
@@ -0,0 +1,5 @@
+INSERT INTO company (CompanyID, CompanyName, CompanyAddress)
+VALUES(1, 'RMIT University Vietnam', 'Handi Resco Building, 521 Kim Ma Street, Ha Noi, Vietnam');
+
+INSERT INTO company (CompanyID, CompanyName, CompanyAddress)
+VALUES (2, 'GPT-5', '702 Nguyen Van Linh Street, District 7, Ho Chi Minh City, Vietnam');
